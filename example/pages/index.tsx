ff4681c9d05cefcd6c335b0f7c9b240fc4c06676
Chris Fields II <chris.fields@thinaer.io>
2021-04-01 00:10:33 -0500

    improve example index page to handle persistRender and toggleDock

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,6 +1,14 @@
 import Head from 'next/head'
 import { useEffect } from 'react'
-import { Container, CloseButton } from '@chakra-ui/react'
+import {
+  Container,
+  CloseButton,
+  Text,
+  Center,
+  Code,
+  Button,
+  VStack,
+} from '@chakra-ui/react'
 
 import { Dock, useDock, DockContainer } from 'react-use-dock'
 
@@ -9,7 +17,7 @@ import { DockControls } from 'comps'
 export default function Home() {
   const dock = useDock()
 
-  useEffect(() => {
+  const openDock = () => {
     dock.openDock({
       render: () => (
         <DockContainer
@@ -19,11 +27,42 @@ export default function Home() {
           <DockControls />
         </DockContainer>
       ),
-      minSize: 350,
-      orientation: 'right',
-      size: 50,
     })
+  }
+
+  useEffect(() => {
+    openDock()
   }, [])
+
+  const onOpen = () => {
+    if (dock.persistRender) return
+
+    dock.setRender(() => (
+      <DockContainer CloseIcon={<CloseButton />}>
+        <Container>
+          <Center>
+            <VStack>
+              <Text>
+                Looks like you closed the Dock with{' '}
+                <Code>persistRender === false</Code>. This page only calls
+                <Code>toggleDock()</Code> which can lead to a blank render in
+                the Dock (this example detected the blank render and instead
+                showed this message). If you close the Dock and{' '}
+                <Code>persistRender === false</Code>, make sure you either use{' '}
+                <Code>{`openDock({ render: () => <YourComponent /> })`}</Code>
+                or use <Code>{`setRender(() => <YourComponent />)`}</Code> and
+                then call <Code>openDock()</Code>.
+              </Text>
+
+              <Button colorScheme="purple" w="full" onClick={openDock}>
+                Reset
+              </Button>
+            </VStack>
+          </Center>
+        </Container>
+      </DockContainer>
+    ))
+  }
 
   return (
     <Container
@@ -40,7 +79,7 @@ export default function Home() {
 
       <Dock />
 
-      {!dock.isOpen && <DockControls />}
+      {!dock.isOpen && <DockControls onOpen={onOpen} />}
     </Container>
   )
 }
